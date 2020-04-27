51a7730534c15afa60bbe7aee2f890ed2e38e474
JasonWang <ff1986755087@gmail.com>
2020-04-28 00:54:18 +0800

    table complete

--- js/all.js
+++ js/all.js
@@ -1,18 +1,24 @@
 
 // 命名
+$('h2').addClass('max1200 titleName');
+
+
 $('header li').addClass('navItem');
+$('.headerText h2').removeClass('max1200 titleName');
+
 
 $('.serviceList li').addClass('serviceListItem');
 $('.serviceList li h3').addClass('serviceListItemTitle');
 $('.serviceList li p').addClass('serviceListItemText');
 
+
 $('.customer li').addClass('customerListCard');
 $('.customer li p').addClass('cardText');
 $('.customer li span').addClass('cardUpdateTime');
 
-$('.footerFlex li').addClass('footerNavItem');
 
-$('h2').addClass('max1200 titleName');
+// $('.footerFlex li').addClass('footerNavItem');
+
 
 $('.classic .classicName').each(function(i){
     n=i+1;
